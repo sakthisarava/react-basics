c6ceb667a2f443a4f3b6a1ad11f503cdfb92435d
sakthi s <velsakthis5301@gmail.com>
2021-12-29 17:00:31 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <div>
+            <h1>This is about us page</h1>
+
+            <buuto>Dashboard</buuto>
+        </div>
+    )
+}
+
+export default About()
